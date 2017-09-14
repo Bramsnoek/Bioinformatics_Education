3967baeb7c4e1f437ef4917b6bdc576c93cf2392
Bram <bramsnoeklith@hotmail.com>
2017-09-14 11:04:19 +0200

    Redownloaded some sequences, added a function to easily plot double bar charts for comparison

--- Fasta_Module/Fasta.py
+++ Fasta_Module/Fasta.py
@@ -11,7 +11,7 @@ class Fasta(dict):
         for file in enumerate(files):
             self.__fasta_to_dict(file[1])
 
-    def get_sequence_length(self, key: object = None) -> object:
+    def get_sequence_length(self, key: object = None) -> [dict, float]:
         """
         :param key: The sequence of which the length is being calculated
         :return: The function returns the length of a single sequence, if no key is provided
@@ -28,7 +28,7 @@ class Fasta(dict):
         except KeyError:
             print('Please provide an existing sequence name')
 
-    def get_gc(self, key: str = None) -> dict:
+    def get_gc(self, key: str = None) -> [dict, float]:
         """
         :param key: The sequence of which the GC% is being calculated
         :return: The function returns the GC% of a single sequence, if no key is provided
--- Fasta_Module/Main.py
+++ Fasta_Module/Main.py
@@ -1,7 +1,7 @@
 from Fasta_Files.File_Manager import FastaFileManager
 from Fasta_Module.Fasta import Fasta
 import matplotlib.pyplot as plt
-from pprint import pprint
+import numpy as np
 
 
 species = ['Arabidopsis_Thaliana',
@@ -11,30 +11,53 @@ species = ['Arabidopsis_Thaliana',
            'Dictyostelium_Discoideum']
 
 
+def show_doublebar_chart(bar_width: float, opacity: float,
+                         first_set_values: list, second_set_values: list, xticks: list, n_groups: int,
+                         title: str, ylabel: str, xlabel: str, first_set_label: str, second_set_label: str):
+    index = np.arange(n_groups)
+
+    plt.bar(index, first_set_values, bar_width,
+            alpha=opacity,
+            color='b',
+            label=first_set_label)
+
+    plt.bar(index + bar_width, second_set_values, bar_width,
+            alpha=opacity,
+            color='g',
+            label=second_set_label)
+
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.xticks(index + bar_width, xticks)
+    plt.legend()
+
+    plt.show()
+
+
 def main():
     file_manager = FastaFileManager(*species)
-    fasta_manager = Fasta(file_manager.get_sequence('mRna_Sequence.fasta'))
-
-    pprint(fasta_manager)
+    fasta_manager = Fasta(file_manager.get_sequence('Rna_Sequence.fasta'))
 
     gc_results = fasta_manager.get_gc()
+    gc_known = [50, 50, 50, 50, 50]
+
     length_results = fasta_manager.get_sequence_length()
+    length_known = [2000, 2000, 2000, 2000, 2000]
 
-    plt.bar(range(len(gc_results)), gc_results.values(), align='center')
-    plt.xticks(range(len(gc_results)), gc_results.keys())
-    plt.title("GC% in different genomes for different organisms")
-    plt.ylabel("Gc% in genome")
-    plt.xlabel("Genome name")
+    show_doublebar_chart(0.4, 0.8, gc_results.values(), gc_known, gc_results.keys(), 5,
+                         "GC% in different genomes for different organisms",
+                         "GC% in genome",
+                         "Genome name",
+                         "Calculated GC%",
+                         "Known GC%")
 
-    plt.show()
-
-    plt.bar(range(len(length_results)), length_results.values(), align='center')
-    plt.xticks(range(len(length_results)), length_results.keys())
-    plt.title("Length of different genomes for different organisms")
-    plt.ylabel("Length of genome sequence")
-    plt.xlabel("Genome name")
-
-    plt.show()
+    show_doublebar_chart(0.4, 0.8, length_results.values(), length_known, length_results.keys(), 5,
+                         "Length of different genomes for different organisms",
+                         "Length of genome",
+                         "Genome name",
+                         "Calculated Length",
+                         "Known Length")
 
 
 if __name__ == '__main__':
